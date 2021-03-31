7d85901b6d0d953e31aaf361253da85036f11712
VarMoon <49207914+VarM00n@users.noreply.github.com>
2021-03-31 18:14:20 +0200

    Zadanie 3

---
+++ LAB_1/Zad_3/server.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>		// inet_ntoa
+#include <time.h> 			// time
+//======================================================================
+int error( int ern, const char *msg ){
+    perror( msg );
+    return ern;
+}
+//======================================================================
+int main( void ) {
+	
+	/* zmienne predefiniowane *****************************************/
+	int  port   = 12347;
+	
+	/* zmienne niezainicjowane ****************************************/
+	int 		n;		
+	int 		fd, newfd;
+	char 		buffer[256];
+	socklen_t 	socklen;
+	
+	struct 	sockaddr_in serv_addr, cli_addr;
+	
+	/* tworzymy gniazdo ***********************************************/
+	fd = socket( AF_INET, SOCK_DGRAM, 0 );		// tworzymy nowe gniazdo
+	if (fd < 0) 
+        return error( 1, "socket()" );
+        
+    /* zapelniamy strukture zerami (inicjujemy) ***********************/
+	bzero( ( char * ) &serv_addr, sizeof( serv_addr ) );	// zapelniamy strukture zerami
+	
+	/* przypisujemy parametry *****************************************/
+	serv_addr.sin_family 	  	= AF_INET;			// typ gniazda 
+	serv_addr.sin_addr.s_addr 	= INADDR_ANY;		// oczekujemy polaczen na kazdym adresie
+	serv_addr.sin_port 			= htons(port);		// port, na ktorym nasluchujemy
+	
+	/* mapujemy gniazdo ***********************************************/
+	if( bind(fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0 ) 
+		return error( 2, "bind()" );
+	
+	/* rozpoczynamy nasluchiwanie na gniezdzie ************************/
+	listen( fd, 5 );
+	
+	/* kod obslugujacy nowe polaczenia ********************************/
+	socklen = sizeof(cli_addr);
+	
+    while( 1 ){
+        
+        printf("Address: %s Port: %d\n",  inet_ntoa( cli_addr.sin_addr ), cli_addr.sin_port );
+			  
+            
+        /* odbieranie informacji, konwersja i tyle ****************/
+        char received_message [200] = "";
+        if ( recv(fd, received_message, 200, 0) < 0 ){
+            return error( 3, "recv()" );
+        }
+        char msg [190] = "";
+        char pid [6] = "";
+        // getting pid
+        for(int i = 0; i < 4; i++){
+            pid[i] = received_message[i];
+        }
+        for(int i = 0, j = 4; j < strlen(received_message); i++, j++){
+            msg[i] = received_message[j];
+        }
+        printf("%s\n", pid);
+        printf("%s\n", msg);
+        FILE * fp;
+            
+        fp = fopen( pid, "w+");
+        fputs(msg, fp);
+            
+        fclose(fp);
+    }
+	 /* zamykamy gniazdo **********************************************/
+	close(fd);
+	
+	//------------------------------------------------------------------
+	return 0; 
+}
+//======================================================================
