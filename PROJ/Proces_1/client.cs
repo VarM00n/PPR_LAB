59bbe76dc718cdf2c6e2568007a910d5e5e43d64
VarMoon <49207914+VarM00n@users.noreply.github.com>
2021-04-28 18:00:29 +0200

    push_zad2

--- PROJ/Proces_1/client.cs
+++ PROJ/Proces_1/client.cs
@@ -11,29 +11,29 @@ namespace socket
     {
         static void Main(string[] args)
         {
-			byte[] bytes = new byte[1024];
-			try {
-				IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
-				IPAddress ipAddress = ipHostInfo.AddressList[0];
-				IPEndPoint remoteEP = new IPEndPoint(ipAddress, 12344);
-				Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Dgram, ProtocolType.Udp );
-				try {
-					sender.Connect(remoteEP);  
-					Console.WriteLine($"Socket connected to {sender.RemoteEndPoint.ToString()}");
-					Console.WriteLine("Enter message");
-					string message = Console.ReadLine();
-					Console.WriteLine("Message: ");
-					Console.WriteLine(message);
+			while (true){
+                byte[] bytes = new byte[1024];
+                try {
+                    IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+                    IPAddress ipAddress = ipHostInfo.AddressList[0];
+                    IPEndPoint remoteEP = new IPEndPoint(ipAddress, 12344);
+                    Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Dgram, ProtocolType.Udp );
+                    try {
+                        sender.Connect(remoteEP);  
+                        Console.WriteLine($"Socket connected to {sender.RemoteEndPoint.ToString()}");
+                        Console.WriteLine("Enter message");
+                        string message = Console.ReadLine();
 
-					byte[] mess = Encoding.ASCII.GetBytes(message);
-					sender.Send(mess);					
-					sender.Shutdown(SocketShutdown.Both);  
-					sender.Close();
-				} catch (Exception e) {
-					Console.WriteLine($"Unexpected exception : {e.ToString()}");
-				}
-			} catch (Exception e) {
-				Console.WriteLine( e.ToString());
+                        byte[] mess = Encoding.ASCII.GetBytes(message);
+                        sender.Send(mess);					
+                        sender.Shutdown(SocketShutdown.Both);  
+                        sender.Close();
+                    } catch (Exception e) {
+                        Console.WriteLine($"Unexpected exception : {e.ToString()}");
+                    }
+                } catch (Exception e) {
+                    Console.WriteLine( e.ToString());
+                }
  			}
         }
     }
